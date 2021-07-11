a7d1087dcf9d4a759634d43a9d7027c1af157c65
t737070 <linegcruz@gmail.com>
2021-07-10 22:39:02 -0300

    Added: Implementation of Splash Screen with their Resources.

--- app/src/main/java/com/alinecruz/to_do_list_dio/SplashActivity.kt
+++ app/src/main/java/com/alinecruz/to_do_list_dio/SplashActivity.kt
@@ -1,16 +1,29 @@
 package com.alinecruz.to_do_list_dio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alinecruz.to_do_list_dio.databinding.ActivitySplashBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class SplashActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashBinding
+    val scope = CoroutineScope(Dispatchers.Main)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        scope.launch {
+            delay(2000)
+            val intent = Intent(application, HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
