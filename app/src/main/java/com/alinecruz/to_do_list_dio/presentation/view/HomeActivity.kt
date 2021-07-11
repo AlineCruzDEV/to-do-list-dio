901cab30018e877c8f1a6a9a6fce112b37d3ba40
t737070 <linegcruz@gmail.com>
2021-07-10 23:00:39 -0300

    Changed: Package Structure.

--- app/src/main/java/com/alinecruz/to_do_list_dio/presentation/view/HomeActivity.kt
+++ app/src/main/java/com/alinecruz/to_do_list_dio/presentation/view/HomeActivity.kt
@@ -1,7 +1,8 @@
-package com.alinecruz.to_do_list_dio
+package com.alinecruz.to_do_list_dio.presentation.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.alinecruz.to_do_list_dio.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
