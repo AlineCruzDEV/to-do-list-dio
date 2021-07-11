25f2d93e39a7bf86aa6cf95bb564d15191b6d34d
t737070 <linegcruz@gmail.com>
2021-07-10 23:08:55 -0300

    Added: binding in Home.

--- app/src/main/java/com/alinecruz/to_do_list_dio/presentation/view/HomeActivity.kt
+++ app/src/main/java/com/alinecruz/to_do_list_dio/presentation/view/HomeActivity.kt
@@ -3,10 +3,15 @@ package com.alinecruz.to_do_list_dio.presentation.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.alinecruz.to_do_list_dio.R
+import com.alinecruz.to_do_list_dio.databinding.ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
